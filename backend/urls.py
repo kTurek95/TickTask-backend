2d978e56f60818f2b46c4ea78295ab4dae484199
Kacper Turek <k.turek1995@gmail.com>
2025-06-13 13:20:14 +0200

    Add grapelli

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,7 @@ from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path("admin/", admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
