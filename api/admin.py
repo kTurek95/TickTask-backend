c76909e487401dac39b078b0073ef4d02be3a24f
Kacper Turek <115101474+kTurek95@users.noreply.github.com>
2025-09-02 22:30:43 +0200

    register Task model with TaskAdmin for better list display and search

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, Group, GroupMembership
+from .models import UserProfile, Group, GroupMembership, Task
 
 # Register your models here.
 
@@ -8,6 +8,10 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role')  # pokazuje te pola w tabeli
     search_fields = ('user__username', 'role')  # umożliwia wyszukiwanie
     
-
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'is_completed', 'created_by', 'assigned_to', 'attachment')
+    search_fields = ('user', 'title', 'created_by', 'assigned_to')
+   
 admin.site.register(Group)
 admin.site.register(GroupMembership)
